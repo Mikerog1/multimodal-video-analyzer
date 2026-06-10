1920b78f04b3646addb5262b67d025445c99aff4
Alec <alec.piz@protonmail.com>
2026-06-10 12:58:17 +0000

    added initial data format for qa pairs

---
+++ benchmarks/__init__.py
@@ -0,0 +1,27 @@
+"""Benchmark data definitions for video question answering."""
+
+from .qa_schema import (
+    AnswerFormat,
+    BenchmarkQADataset,
+    BenchmarkQAPair,
+    DayNightTag,
+    DifficultyLevel,
+    EvidenceSpan,
+    QAFamily,
+    ReasoningType,
+    TrajectoryLinkage,
+    VisibilityQuality,
+)
+
+__all__ = [
+    "AnswerFormat",
+    "BenchmarkQADataset",
+    "BenchmarkQAPair",
+    "DayNightTag",
+    "DifficultyLevel",
+    "EvidenceSpan",
+    "QAFamily",
+    "ReasoningType",
+    "TrajectoryLinkage",
+    "VisibilityQuality",
+]
